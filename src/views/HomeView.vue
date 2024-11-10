7b674a7395274e2852ec64e00056445d0f441ca4
Emman <emman.bg6@gmail.com>
2024-11-10 17:33:07 +0800

    Fix login and add sidebar in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <template>
-    <p>Hello world!</p>
+    <v-app>
+        <v-navigation-drawer
+            class="bg-purple-darken-1"
+            theme="dark"
+            expand-on-hover
+            rail
+        >
+            <v-list-item
+                prepend-icon="mdi-account-circle-outline"
+                :title="`Welcome, ${userProfile.username}!`"
+            >
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-item prepend-icon="mdi-home" link title="Home"></v-list-item>
+            <v-list-item prepend-icon="mdi-television-play" link title="My Channel"></v-list-item>
+            <template v-slot:append>
+                <v-list-item prepend-icon="mdi-logout" link title="Logout"></v-list-item>
+            </template>
+        </v-navigation-drawer>
+    </v-app>
 </template>
 
 <script>
@@ -9,10 +28,19 @@ import { defineComponent } from 'vue';
 // import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
-  name: 'HomeView',
+    name: 'HomeView',
 
-  components: {
-    // HelloWorld,
-  },
+    data() {
+        return {
+            userProfile: {
+                username: 'brownie05'
+            }
+        }
+    },
+
+
+    components: {
+        // HelloWorld,
+    },
 });
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,123 @@
 <template>
-    <v-sheet class="bg-deep-purple pa-12" rounded>
-        <v-card class="mx-auto px-6 py-8" max-width="344">
-            <v-form
-                v-model="form"
-                @submit.prevent="login"
+    <div>
+        <v-banner
+            v-if="errorMessage"
+            class="mx-auto my-4 text-red"
+            border="error md"
+            color="red"
+            icon="mdi-lock"
+            lines="one"
+            max-width="448"
+            rounded="lg"
+            elevation="8"
+        >
+            <v-banner-text>
+                {{ errorMessage }}
+            </v-banner-text>
+
+            <template v-slot:actions>
+                <v-btn @click="errorMessage = ''">Dismiss</v-btn>
+            </template>
+        </v-banner>
+        <v-img
+            class="mx-auto my-6"
+            max-width="128"
+            src="../assets/stream.svg"
+        ></v-img>
+        <v-card
+            class="mx-auto pa-12 pb-8"
+            elevation="8"
+            max-width="448"
+            rounded="lg"
+        >
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+        <v-text-field
+            v-model="email"
+            density="compact"
+            placeholder="Email address"
+            prepend-inner-icon="mdi-email-outline"
+            variant="outlined"
+        ></v-text-field>
+
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+            Password
+
+            <a
+                class="text-caption text-decoration-none text-blue"
+                href="#"
+                rel="noopener noreferrer"
+                target="_blank"
             >
-                <v-text-field
-                    v-model="email"
-                    :readonly="loading"
-                    :rules="[required]"
-                    class="mb-2"
-                    label="Email"
-                    clearable
-                ></v-text-field>
+            Forgot login password?</a>
+        </div>
 
-                <v-text-field
-                    v-model="password"
-                    :readonly="loading"
-                    :rules="[required]"
-                    type="password"
-                    label="Password"
-                    placeholder="Enter your password"
-                    clearable
-                ></v-text-field>
+        <v-text-field
+            v-model="password"
+            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'"
+            density="compact"
+            placeholder="Enter your password"
+            prepend-inner-icon="mdi-lock-outline"
+            variant="outlined"
+            @click:append-inner="visible = !visible"
+        ></v-text-field>
 
-                <br>
-
-                <v-btn
-                    :disabled="!form"
-                    :loading="loading"
-                    color="success"
-                    size="large"
-                    type="submit"
-                    variant="elevated"
-                    block
-                >
-                Sign In
-                </v-btn>
-            </v-form>
+        <v-card
+            class="mb-12"
+            color="surface-variant"
+            variant="tonal"
+        >
+            <v-card-text class="text-medium-emphasis text-caption">
+                Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
+            </v-card-text>
         </v-card>
-    </v-sheet>
-</template>
 
+        <v-btn
+            class="mb-8"
+            color="blue"
+            size="large"
+            variant="tonal"
+            block
+            @click="login"
+        >
+            Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+            <a
+                class="text-blue text-decoration-none"
+                href="#"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+            </a>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
 <script>
     import axios from 'axios'
 
     export default {
-        data () {
-            return {
-                form: false,
-                email: null,
-                password: null,
-                loading: false,
+        data: () => ({
+            visible: false,
+            email: null,
+            password: null,
+            errorMessage: ''
+        }),
+
+        watch: {
+            errorMessage() {
+                if (this.errorMessage) {
+                    setTimeout(() => {
+                        this.errorMessage = '';
+                    }, 5000);
+                }
             }
         },
+
         methods: {
             async login () {
                 try {
@@ -62,29 +126,16 @@
                     password: this.password
                 })
                 localStorage.setItem('access_token', response.data.access)
-                console.log('Login successful:', response.data)
-                } catch (error) {
-                this.errorMessage = 'Invalid email or password'
-                console.error('Login error:', error)
+                this.$router.push({name: 'home'})
+                } catch(error) {
+                    this.errorMessage = 'Invalid email or password'
+                    console.error('Login error:', error)
+                    this.errorMessage = 'Invalid email or password.';
                 }
-            },
-
-            onSubmit () {
-                if (!this.form) return
-
-                this.loading = true
-
-                setTimeout(() => (this.loading = false), 2000)
-                },
-                required (v) {
-                return !!v || 'Field is required'
             },
         }
     }
-</script>
 
-<style>
-    .error {
-    color: red;
-    }
+</script>
+<style scoped>
 </style>
