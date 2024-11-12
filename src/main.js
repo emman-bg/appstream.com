ceca3fbdd1b4c56a4a818d409c31685dbefe5355
Emman <emman.bg6@gmail.com>
2024-11-13 01:57:29 +0800

    Add api as global for interceptors

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
+import store from './store';
+import api from './services/api';
 
-loadFonts()
+loadFonts();
 
-createApp(App).use(store)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App);
+
+app.config.globalProperties.$api = api;
+
+app.use(router);
+app.use(vuetify);
+app.use(store);
+app.mount('#app');
